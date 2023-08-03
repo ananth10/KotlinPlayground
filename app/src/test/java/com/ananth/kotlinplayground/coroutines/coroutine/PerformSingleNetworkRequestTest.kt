6eccc92006529c56215bd83dc0199c6654a11362
Ananth <ananthababu.vannimuthu@abbvie.com>
2023-08-03 19:32:25 +0530

    Done unit test for single network request using coroutine

--- app/src/test/java/com/ananth/kotlinplayground/coroutines/coroutine/PerformSingleNetworkRequestTest.kt
+++ app/src/test/java/com/ananth/kotlinplayground/coroutines/coroutine/PerformSingleNetworkRequestTest.kt
@@ -1,30 +1,22 @@
 package com.ananth.kotlinplayground.coroutines.coroutine
 
-import android.util.Log
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.ananth.kotlinplayground.utils.MainCoroutineScopeRule
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.setMain
-import org.junit.After
 import org.junit.Assert
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
 
 
 internal class PerformSingleNetworkRequestTest {
+    val testDispatcher = TestCoroutineDispatcher()
 
-    @Before
-    fun setUp() {
-    }
-
-    @After
-    fun tearDown() {
-    }
+    @get:Rule
+    val  mainCoroutineDispatcher = MainCoroutineScopeRule()
 
     @get:Rule //this rule define executor instead of android main looper and this also configures live data to execute task synchronously instead of asynchronously because if we try to  test any async functionality then assert will execute before async function happens, so that is why we are using InstantTaskExecutorRule to execute async to sync
     val testInstantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
@@ -35,8 +27,7 @@ internal class PerformSingleNetworkRequestTest {
     @Test
     fun `should return success when network request is successful`() {
         //Arrange
-        val testDispatcher = TestCoroutineDispatcher()
-        Dispatchers.setMain(testDispatcher)
+
         val fakeApi = FakeSuccessApi()
         val viewModel = PerformSingleNetworkRequest(fakeApi)
 
@@ -59,7 +50,6 @@ internal class PerformSingleNetworkRequestTest {
     @Test
     fun `should return error when network request fails`() {
         //Arrange
-        Dispatchers.setMain(TestCoroutineDispatcher())
         val fakeApi = FakeErrorApi()
         val viewModel = PerformSingleNetworkRequest(fakeApi)
 
---
+++ app/src/test/java/com/ananth/kotlinplayground/utils/MainCoroutineScopeRule.kt
@@ -0,0 +1,23 @@
+package com.ananth.kotlinplayground.utils
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.*
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+class MainCoroutineScopeRule(
+    val testDispatcher: TestDispatcher = TestCoroutineDispatcher()
+): TestWatcher(), TestCoroutineScope by TestCoroutineScope(testDispatcher){
+
+    override fun starting(description: Description) {
+        super.starting(description)
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        super.finished(description)
+        cleanupTestCoroutines()
+        Dispatchers.resetMain()
+
+    }
+}
