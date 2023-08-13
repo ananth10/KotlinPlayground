93e79bddddb955bb84fc0ba4ddb524b99c6692d7
Ananth <ananthababu.vannimuthu@abbvie.com>
2023-08-13 10:48:36 +0530

    learning annotations

--- app/src/main/java/com/ananth/kotlinplayground/annotations_reflections/Annotations.kt
+++ app/src/main/java/com/ananth/kotlinplayground/annotations_reflections/Annotations.kt
@@ -61,7 +61,7 @@ fun testRemove(){
  *   ->To use a property as an annotation argument, you need to mark it with a const modifier,
  *   which tells the compiler that the property is a compile-time constant.
  *
- *   ->ere’s an example of JUnit’s @Test annotation that specifies the timeout for the test, in milliseconds, using the timeout parameter:
+ *   ->there’s an example of JUnit’s @Test annotation that specifies the timeout for the test, in milliseconds, using the timeout parameter:
  * */
 
 const val TEST_TIMEOUT = 100L
@@ -70,3 +70,106 @@ const val TEST_TIMEOUT = 100L
 
 
 //10.1.2. Annotation targets
+/**
+ * ->In many cases, a single declaration in the Kotlin source code corresponds to multiple Java declarations, and each of them can carry annotations.
+ * ->For example, a Kotlin property corresponds to a Java field, a getter, and possibly a setter and its parameter.
+ * ->A property declared in the primary constructor has one more corresponding element: the constructor parameter.
+ * Therefore, it may be necessary to specify which of these elements needs to be annotated.
+ *
+ * ->You specify the element to be annotated with a use-site target declaration.
+ * ->The use-site target is placed between the @ sign and the annotation name and is separated from the name with a colon.
+ * ->The word get in figure 10.1 causes the annotation @Rule to be applied to the property getter. @get:Rule
+ *
+ * ->In JUnit, you can specify a rule to be executed before each test method.
+ * ->For instance, the standard TemporaryFolder rule is used to create files and folders that are deleted when the test method finishes.
+ * ->To specify a rule, in Java you declare a public field or method annotated with @Rule.
+ *  But if you just annotate the property folder in your Kotlin test class with @Rule
+ *  you’ll get a JUnit exception: “The @Rule ‘folder’ must be public.” It happens because @Rule is applied to the field,
+ *   which is private by default. To apply it to the getter, you need to write that explicitly, @get:Rule, as follows:
+ *
+ *  ->If you annotate a property with an annotation declared in Java, it’s applied to the corresponding field by default
+ *  Kotlin also lets you declare annotations that can be directly applied to properties.
+ *
+ *  The full list of supported use-site targets is as follows:
+ *
+ * 1.property—Java annotations can’t be applied with this use-site target.
+   2.field—Field generated for the property.
+   3.get—Property getter.
+   4.set—Property setter.
+   5.receiver—Receiver parameter of an extension function or property.
+   6.param—Constructor parameter.
+   7.setparam—Property setter parameter.
+   8,delegate—Field storing the delegate instance for a delegated property.
+   9.file—Class containing top-level functions and properties declared in the file.
+
+ ->Any annotation with the file target needs to be placed at the top level of the file,  before the package directive.
+One of the annotations commonly applied to files is @JvmName, which changes the name of the corresponding class. Section 3.2.3 showed you an example:
+@file:JvmName("StringFunctions").
+ *
+ * ->Note that unlike Java, Kotlin allows you to apply annotations to arbitrary expressions, not only to class and function declarations or types
+ *  The most common example is the @Suppress annotation,
+ *  which you can use to suppress a specific compiler warning in the context of the annotated expression
+ *
+ *  Here’s an example that annotates a local variable declaration to suppress an unchecked cast warning:
+ *
+ * */
+class HasTempFolder {
+//    @get:Rule
+//    val folder = TemporaryFolder()
+//    @Test
+//    fun testUsingTempFolder() {
+//        val createdFile = folder.newFile("myfile.txt")
+//        val createdFolder = folder.newFolder("subfolder")
+//        // ...
+//    }
+}
+
+
+fun test(list: List<*>) {
+    @Suppress("UNCHECKED_CAST")
+    val strings = list as List<String>
+    // ...
+}
+
+//CONTROLLING THE JAVA API WITH ANNOTATIONS
+/**
+ * ->Kotlin provides a variety of annotations to control how declarations written in Kotlin are compiled to Java bytecode and exposed to Java callers.
+ * ->Some of those annotations replace the corresponding keywords of the Java language:
+ * ->for example, the @Volatile and @Strictfp annotations serve as direct replacements for Java’s volatile and strictfp keywords.
+ * Others are used to change how Kotlin’s declarations are visible to Java callers:
+ * 1.@JvmName changes the name of a Java method or field generated from a Kotlin declaration.
+ * 2.@JvmStatic can be applied to methods of an object declaration or a companion object to expose them as static Java methods.
+ * 3.@JvmOverloads, mentioned in section 3.2.2, instructs the Kotlin compiler to generate overloads for a function that has default parameter values.
+ * 4.@JvmField can be applied to a property to expose that property as a public, Java field with no getters or setters.
+ * */
+
+//10.1.3. Using annotations to customize JSON serialization
+/**
+ * ->One of the classic use cases for annotations is customizing object serialization
+ * Serialization:
+ * Serialization is a process of converting an object to a binary or text representation that can be then stored or sent over the network.
+ * The reverse process, deserialization,
+ * converts such a representation back to an object.
+ * One of the most common formats used for serialization is JSON
+ * ->There are many widely used libraries for serializing Java objects to JSON, including Jackson, Gson
+ * */
+
+/**
+ * ->You can use annotations to customize the way objects are serialized and deserialized.
+ * ->When serializing an object to JSON, by default the library tries to serialize all the properties and uses the property names as keys.
+ * ->The annotations allow you to change the defaults.
+ * ->The @JsonExclude annotation is used to mark a property that should be excluded from serialization and deserialization.
+ * ->The @JsonName annotation lets you specify that the key in the key/value pair representing the property should be the given string, not the name of the property.
+ * */
+
+//data class Person(
+//    @JsonName("alias") val firstName: String,
+//    @JsonExclude val age: Int? = null
+//)
+/**
+ * ->You annotate the property firstName to change the key used to represent it in JSON.
+ * ->You annotate the property age to exclude it from serialization and deserialization.
+ * ->Note that you must specify the default value of the property age. Otherwise, you wouldn’t be able to create a new instance of Person during deserialization.
+ * */
+
+//10.1.4. Declaring annotations
